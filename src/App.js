2e9c6fe408189b9ae69a537e03e9dc8fb40fec19
Lucas <lucas.systema@gmail.com>
2023-09-23 16:43:54 -0300

    Front improvements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import { apiClient } from './services/api';
 import React, { useEffect, useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Modal, TablePagination, TextField, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, CardMedia, Container, Grid, Modal, TextField, Typography } from '@mui/material';
+import { CircularProgress, Pagination, Stack } from '@mui/material';
 import './style.css';
 
 function App() {
 
   const [users, setUsers] = useState([])
 
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [totalResults, setTotalResults] = useState(0);
   const [searchName, setSearchName] = useState('');
   const [searchState, setSearchState] = useState('');
   const [userModal, setUserModal] = useState({});
+  const [loading, setLoading] = useState(false);
   const [open, setOpen] = React.useState(false);
+
   const handleOpen = (user) => {
     setUserModal(user)
     setOpen(true)
   };
+
   const handleClose = () => {
     setUserModal({})
     setOpen(false)
@@ -27,27 +32,40 @@ function App() {
     setPage(page);
   };
 
-  const handleChangeRowsPerPage = (event) => {
+  const handleChangeRowsPerPage = async (event) => {
     setRowsPerPage(+event.target.value);
-    setPage(0);
+    //setPage(0);
   };
 
-  const getInfo = async (type = null) => {
+  const getInfo = (type = null) => {
     if (type) {
       setSearchName('')
       setSearchState('')
     }
     setUsers([])
-    //apiClient.httpInstance.get("/products.json?brand=covergirl&product_type=lipstick", {})
+    setLoading(true)
+
     apiClient.httpInstance.get("/users", {
       params: {
         "name": type ? '' : searchName,
         "state": type ? '' : searchState,
+        "page": page,
+        "limit": rowsPerPage,
       }
     })
-      .then(async response => {
+      .then(response => {
         if (response.status === 200) {
-          setUsers(response.data)
+
+          setUsers(response.data.users)
+          setTotalResults(response.data.totalRegisters)
+
+          if (response.data?.pagination?.next?.page) {
+            setPage(response.data.pagination.next.page - 1)
+          } else {
+            setPage(1)
+          }
+
+          setLoading(false)
         }
       })
       .catch(error => {
@@ -57,27 +75,32 @@ function App() {
 
   useEffect(() => {
     getInfo()
-  }, []);
+  }, [rowsPerPage, page]);
 
   return (
     <>
-      <Container maxWidth="md" className="body" >
-        <Typography color="primary" variant="h2" component="h2">Usuários</Typography>
-        <p>
-          <Box
-            component="form"
-            sx={{
-              '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField id="search-name" label="Nome" variant="outlined" color="secondary" size="small" value={searchName} onChange={ev => setSearchName(ev.target.value)} />
-            <TextField id="search-state" label="Estado" variant="outlined" color="secondary" size="small" value={searchState} onChange={ev => setSearchState(ev.target.value)} />
-            <span><Button variant="outlined" onClick={() => getInfo()}>Pesquisar</Button></span>
-            <span><Button variant="outlined" onClick={() => getInfo('clean')}>Limpar</Button></span>
-          </Box>
-        </p>
+      <Container maxWidth="md" className="body">
+        <Grid container spacing={2} padding={4} justifyContent={'center'}>
+          <Typography color="primary" variant="h2" component="h2">Usuários</Typography>
+        </Grid>
+        <Grid container spacing={2} padding={4} justifyContent={'center'}>
+          <Grid container xs={12} md={8} spacing={2} justifyContent={'left'}>
+            <Grid item xs={12} md={5}>
+              <TextField id="search-name" label="Nome" variant="outlined" color="secondary" size="small" value={searchName} onChange={ev => setSearchName(ev.target.value)} />
+            </Grid>
+            <Grid item xs={12} md={5}>
+              <TextField id="search-state" label="Estado" variant="outlined" color="secondary" size="small" value={searchState} onChange={ev => setSearchState(ev.target.value)} />
+            </Grid>
+          </Grid>
+          <Grid container xs={12} md={4} spacing={2} justifyContent={'right'}>
+            <Grid item>
+              <span><Button variant="outlined" onClick={() => getInfo()}>Pesquisar</Button></span>
+            </Grid>
+            <Grid item>
+              <span><Button variant="outlined" onClick={() => getInfo('clean')}>Limpar</Button></span>
+            </Grid>
+          </Grid>
+        </Grid>
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} size="small" aria-label="a table">
             <TableHead>
@@ -89,40 +112,51 @@ function App() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {users
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                      <TableCell key={row.id}>
-                        {row.name.first} {row.name.last}
-                      </TableCell>
-                      <TableCell key={row.id}>
-                        {row.email}
-                      </TableCell>
-                      <TableCell key={row.id}>
-                        {row.location.state}
-                      </TableCell>
-                      <TableCell key={row.id}>
-                        <Button onClick={() => handleOpen(row)}>Ver</Button>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
+              {users.map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                    <TableCell key={row.id}>
+                      {row.name.first.toUpperCase()} {row.name.last.toUpperCase()}
+                    </TableCell>
+                    <TableCell key={row.id}>
+                      {row.email}
+                    </TableCell>
+                    <TableCell key={row.id}>
+                      {row.location.state.toUpperCase()}
+                    </TableCell>
+                    <TableCell key={row.id}>
+                      <Button onClick={() => handleOpen(row)}>Exibir</Button>
+                      <Button onClick={() => handleOpen(row)}>Abrir</Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })
+              }
             </TableBody>
           </Table>
         </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 50, { label: 'All', value: -1 }]}
-          component="div"
-          count={users.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
 
-      </Container>
+        {!users.length &&
+          <Grid container direction="row" justifyContent="center" alignItems="center" bgcolor={'#fff'}
+            sx={{
+              height: 350,
+            }}>
+            <Grid item>
+              {loading ? <CircularProgress size={30} /> : <Typography color="secondary" variant="h6">Nenhum usuário encontrado</Typography>}
+            </Grid>
+          </Grid>
+        }
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Stack spacing={2} padding={3}>
+            <Pagination count={(totalResults / 10) % 1 !== 0 ? Math.trunc(totalResults / 10) + 1 : Math.trunc(totalResults / 10)} page={page} onChange={handleChangePage} color="primary" />
+          </Stack>
+        </Grid>
+      </Container >
       <Modal
         open={open}
         onClose={handleClose}
@@ -141,12 +175,12 @@ function App() {
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                {userModal?.name?.first} {userModal?.name?.last}
+                {userModal?.name?.first.toUpperCase()} {userModal?.name?.last.toUpperCase()}
               </Typography>
               <Typography sx={{ mt: 2 }}>
                 <Typography variant="h6" component="h6">
                   Estado
-                </Typography> {userModal?.location?.state}
+                </Typography> {userModal?.location?.state.toUpperCase()}
               </Typography>
               <Typography sx={{ mt: 2 }}>
                 <Typography variant="h6" component="h6">
